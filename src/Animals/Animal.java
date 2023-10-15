25613ecf5d538597dc3bbb951091f03ccc0456d0
wiktordablju <wiktordablju@gmail.com>
2023-10-15 16:36:44 +0200

    15.10: Zadanie domowe obiektowosc

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -1,12 +1,23 @@
 package Animals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Animal {
+
     String race;
     String name;
+
+//   Musi byc public static zeby wywolac w mainie
+    public static List<Animal> animalsList = new ArrayList<>();
+//  List<Co dodajemy (String, Int, dany obiekt)> nazwaListy = new ArrayList<>();
+
 
     public Animal(String race, String name) {
         this.race = race;
         this.name = name;
+//      this odnosi sie do obiektu ktorego wywolujemy, dodaje go z automatu do listy animalsow
+        animalsList.add(this);
     }
 
     public String getRace() {
@@ -27,5 +38,9 @@ public class Animal {
 
     public void makeNoise() {
         System.out.println("Wydaje odglos");
+    }
+
+    public void displayName() {
+        System.out.println("Moje imie to: " + name);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+// 1. Roznice miedzy arrayami, listami, setami a mapami
+// 2. pies pies = new pies()
+// 3. usemakenoise
+// 4. DisplayName wszystkich zwierzakow (w forze)
+
 import Animals.Animal;
 import Animals.Doggo;
 import Animals.Cat;
@@ -7,18 +12,21 @@ public class Main {
     public static void main(String[] args) {
 
         Doggo szarik = new Doggo("Owczarek", "Szarik");
-        useMakeNoise(szarik);
-
         Cat mruczek = new Cat("Dachowiec", "Mruczek");
-        useMakeNoise(mruczek);
-
         AntEater kamil = new AntEater("Masz jakas mruwke?", "Kamil");
-        useMakeNoise(kamil);
 
-
+//      Pierwszy Animal odnosi sie do typu zmiennych(moze byc tez String, whatever)
+        for (Animal animal : Animal.animalsList) {
+            useDisplayName(animal);
+            useMakeNoise(animal);
+        }
     }
 
     public static void useMakeNoise(Animal animal) {
         animal.makeNoise();
+    }
+
+    public static void useDisplayName(Animal animal) {
+        animal.displayName();
     }
 }
